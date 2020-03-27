1fb60cfdce4ffa11334774318a0752fb18d8a03e
venkatmithun <venkatmithun.k@gmail.com>
2020-03-27 08:18:12 -0700

    Updates

---
+++ precache-manifest.3d0cc59419fcd364b2a03bc8181f9f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc77ddcec915b4df83c86f8b7c457ae0",
+    "url": "/venkatmithun/pubmed-database/index.html"
+  },
+  {
+    "revision": "a172efe10782357f208e",
+    "url": "/venkatmithun/pubmed-database/static/css/main.a701dd7d.chunk.css"
+  },
+  {
+    "revision": "cc5480e8bd5230c962a5",
+    "url": "/venkatmithun/pubmed-database/static/js/2.2e183f3e.chunk.js"
+  },
+  {
+    "revision": "defd798cd79a0093d17ca840ee856f4b",
+    "url": "/venkatmithun/pubmed-database/static/js/2.2e183f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c3619ea4aee5feceae",
+    "url": "/venkatmithun/pubmed-database/static/js/3.2169aee5.chunk.js"
+  },
+  {
+    "revision": "a172efe10782357f208e",
+    "url": "/venkatmithun/pubmed-database/static/js/main.b9bb828c.chunk.js"
+  },
+  {
+    "revision": "bbd79ea46d18d01b89d6",
+    "url": "/venkatmithun/pubmed-database/static/js/runtime-main.5757a0e1.js"
+  }
+]);
